cd572949bbaa950dace75206516f1c05b7c11257
AWildErin <sharpian126@gmail.com>
2025-09-22 00:24:21 +0100

    feat: Add a child entities dict on Entity

--- CBRoomConverter/Models/Entity.cs
+++ CBRoomConverter/Models/Entity.cs
@@ -8,20 +8,8 @@ internal class Entity
 	public ESCPCBRoomCreatorEntityType Type { get; set; } = ESCPCBRoomCreatorEntityType.None;
 	public string? Name { get; set; }
 	public Dictionary<string, string> Properties { get; set; } = new();
+	public Dictionary<string, Entity> ChildEntities { get; set; } = new();
 
 	[JsonIgnore]
 	public Room? OwnerRoom { get; set; }
-
-	// Inefficient and lazy but gets the job done
-	// Took this suggestion from a reddit post
-	//public Entity Copy()
-	//{
-	//	var ent = JsonSerializer.Deserialize<Entity>( JsonSerializer.Serialize( this ) );
-	//	if (ent is null)
-	//	{
-	//		throw new Exception( $"Failed to copy {Name}" );
-	//	}
-
-	//	return ent;
-	//}
 }
