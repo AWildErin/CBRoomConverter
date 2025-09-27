ff65376163276a3fce3c39a17f3a4aa00292b847
AWildErin <sharpian126@gmail.com>
2025-09-27 11:32:33 +0100

    feat: Add initial tests project

---
+++ CBRoomConverter.Tests/TestHelpers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+namespace CBRoomConverter.Tests;
+
+internal static class TestHelpers
+{
+	public static string GetTestResource( string ResourcePath )
+	{
+		// @todo This isn't ideal, but for my usecase it's fne
+		string exeDir = Directory.GetCurrentDirectory();
+		string testResourcePath = Path.Combine( exeDir, "../../../TestResources" );
+		return Path.GetFullPath( Path.Combine( testResourcePath, ResourcePath ) );
+	}
+}
---
+++ CBRoomConverter/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible( false )]
+[assembly: Guid( "5eec9f37-6bbb-4b6d-99cb-b129776e7d21" )]
+
+[assembly: InternalsVisibleTo( "CBRoomConverter.Tests" )]
