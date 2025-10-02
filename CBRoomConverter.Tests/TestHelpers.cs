793dad456fb6f5c22e4a869e270b2e13f5365888
AWildErin <sharpian126@gmail.com>
2025-10-02 01:04:45 +0100

    fix: Correct path for test resource helpers

--- CBRoomConverter.Tests/TestHelpers.cs
+++ CBRoomConverter.Tests/TestHelpers.cs
@@ -8,7 +8,7 @@ internal static class TestHelpers
 	{
 		// @todo This isn't ideal, but for my usecase it's fne
 		string exeDir = Directory.GetCurrentDirectory();
-		string testResourcePath = Path.Combine( exeDir, "../../../TestResources" );
+		string testResourcePath = Path.Combine( exeDir, "../../../../TestResources" );
 		return Path.GetFullPath( Path.Combine( testResourcePath, ResourcePath ) );
 	}
 }
